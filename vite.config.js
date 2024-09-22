d4899feb554c04c7757f0ef1380aee86a20001df
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2024-09-22 17:10:54 +0500

    s

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',  // Ensures assets are served from relative paths
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
+  },
+});
